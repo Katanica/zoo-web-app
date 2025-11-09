6f894aa4d631f059974251f3fc51b1058f5748c2
marioljusanin <mario.ljusanin@fsre.sum.ba>
2025-11-09 19:21:45 +0100

    brisanje zivotinja

--- zoo-web-app/src/main/java/zoo_web_app/Controller/ZivotinjaController.java
+++ zoo-web-app/src/main/java/zoo_web_app/Controller/ZivotinjaController.java
@@ -1,4 +1,6 @@
 package zoo_web_app.Controller;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import zoo_web_app.Entity.Zivotinja;
@@ -14,12 +16,22 @@ import java.util.Optional;
 public class ZivotinjaController {
     @Autowired
     private ZivotinjaRepository repo;
+
     @GetMapping
-    public List<Zivotinja> getAll() { return repo.findAll(); }
+    public List<Zivotinja> getAll() {
+        return repo.findAll();
+    }
+
     @GetMapping("/{id}")
-    public Optional<Zivotinja> getById(@PathVariable Long id) { return repo.findById(id); }
+    public Optional<Zivotinja> getById(@PathVariable Long id) {
+        return repo.findById(id);
+    }
+
     @PostMapping
-    public Zivotinja create(@RequestBody Zivotinja z) { return repo.save(z); }
+    public Zivotinja create(@RequestBody Zivotinja z) {
+        return repo.save(z);
+    }
+
     @PutMapping("/{id}")
     public Zivotinja update(@PathVariable Long id, @RequestBody Zivotinja nova) {
         return repo.findById(id).map(z -> {
@@ -34,8 +46,21 @@ public class ZivotinjaController {
             return repo.save(nova);
         });
     }
+    @GetMapping("/aktivne")
+    public List<Zivotinja> getActive() {
+        return repo.findByAktivnaTrue();
+    }
+
+
     @DeleteMapping("/{id}")
-    public void delete(@PathVariable Long id) { repo.deleteById(id); }
+    public ResponseEntity<String> deactivate(@PathVariable Long id) {
+        return repo.findById(id).map(zivotinja -> {
+            zivotinja.setAktivna(false);
+            repo.save(zivotinja);
+            return ResponseEntity.ok("Životinja je uspješno deaktivirana (označena kao neaktivna).");
+        }).orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).body("Životinja nije pronađena."));
+    }
 }
+
 
 
