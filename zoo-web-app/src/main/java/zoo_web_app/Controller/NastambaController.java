36bc55c983b8f04e41e6f5212b18fb8c1676e975
Katanica <gabrijelkatangk@gmail.com>
2025-11-10 16:21:40 +0100

    gitignore i povezivanje par stranica

--- zoo-web-app/src/main/java/zoo_web_app/Controller/NastambaController.java
+++ zoo-web-app/src/main/java/zoo_web_app/Controller/NastambaController.java
@@ -13,7 +13,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api/nastamba")
+@RequestMapping("/nastamba")
 public class NastambaController {
     @Autowired
     private NastambaRepository nastambaRepository;
