cea2d4f796e88590422f1d817b9c102901caa1ab
Marko Samardzic <samardzicm33@gmail.com>
2025-11-13 21:09:56 +0100

    WIP: zapoceo service i implementacije uz controller

---
+++ zoo-web-app/src/main/java/zoo_web_app/Service/impl/JedinkaServiceImpl.java
@@ -0,0 +1,56 @@
+package zoo_web_app.Service.impl;
+
+import zoo_web_app.Entity.Jedinka;
+import zoo_web_app.Repository.JedinkaRepository;
+import zoo_web_app.Service.JedinkaService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class JedinkaServiceImpl implements JedinkaService {
+
+    private final JedinkaRepository jedinkaRepository;
+
+    public JedinkaServiceImpl(JedinkaRepository jedinkaRepository) {
+        this.jedinkaRepository = jedinkaRepository;
+    }
+
+    @Override
+    public List<Jedinka> findAll() {
+        return jedinkaRepository.findAll();
+    }
+
+    @Override
+    public Jedinka findById(Long id) {
+        return jedinkaRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Jedinka nije pronađena: " + id));
+    }
+
+    @Override
+    public Jedinka create(Jedinka jedinka) {
+        return jedinkaRepository.save(jedinka);
+    }
+
+    @Override
+    public Jedinka update(Long id, Jedinka updated) {
+        Jedinka j = findById(id);
+
+        j.setVrsta(updated.getVrsta());
+        j.setNastamba(updated.getNastamba());
+        j.setNacinNabave(updated.getNacinNabave());
+        j.setNadimak(updated.getNadimak());
+        j.setAktivna(updated.isAktivna());
+        j.setDatumNabave(updated.getDatumNabave());
+        j.setDatumNeaktivnosti(updated.getDatumNeaktivnosti());
+        j.setRazlogNeaktivnosti(updated.getRazlogNeaktivnosti());
+
+        return jedinkaRepository.save(j);
+    }
+
+    @Override
+    public void delete(Long id) {
+        Jedinka j = findById(id);
+        jedinkaRepository.delete(j);
+    }
+}
