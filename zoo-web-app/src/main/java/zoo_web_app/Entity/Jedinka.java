794d355f5e0b159e8074cb4f75066dd0f2826436
Katanica <gabrijelkatangk@gmail.com>
2025-11-22 17:23:40 +0100

    Kompletna funkcionalnost ZIVOTINJE

--- zoo-web-app/src/main/java/zoo_web_app/Entity/Jedinka.java
+++ zoo-web-app/src/main/java/zoo_web_app/Entity/Jedinka.java
@@ -1,5 +1,6 @@
 package zoo_web_app.Entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.*;
 import java.time.LocalDate;
@@ -17,22 +18,30 @@ public class Jedinka {
     @Column(name = "id_jedinke")
     private Long id;
 
-    @ManyToOne
-    @JoinColumn(name = "id_vrste", nullable = false)
-    private VrstaZivotinje vrsta;
-
-    @ManyToOne
-    @JoinColumn(name = "id_nastambe", nullable = false)
-    private Nastamba nastamba;
-
-    @ManyToOne
-    @JoinColumn(name = "id_nabave", nullable = false)
-    private NacinNabave nacinNabave;
-
     @Column(name = "nadimak")
     private String nadimak;
 
-    @Column(name = "aktivna", nullable = false)
+    @Column(name = "identifikator"/*, nullable = false*/)
+    private String identifikator;
+
+    @Column(name = "latinski_naziv"/*, nullable = false*/)
+    private String latinskiNaziv;
+
+    @Column(name = "hrvatski_naziv"/*, nullable = false*/)
+    private String hrvatskiNaziv;
+
+    @ManyToOne
+    @JoinColumn(name = "id_nastambe"/*, nullable = false*/)
+    private Nastamba nastamba;
+
+    @ManyToOne
+    @JoinColumn(name = "id_nabave"/*, nullable = false*/)
+    private NacinNabave nacinNabave;
+
+    @Column(name="link")
+    private String link;
+
+    @Column(name = "aktivna"/*, nullable = false*/)
     private boolean aktivna = true;
 
     @Column(name = "datum_nabave")
@@ -45,11 +54,14 @@ public class Jedinka {
     private String razlogNeaktivnosti;
 
     @OneToMany(mappedBy = "jedinka")
+    @JsonIgnore
     private List<Obaveza> obaveze;
 
     @OneToMany(mappedBy = "jedinka")
+    @JsonIgnore
     private List<Trosak> troskovi;
 
     @OneToMany(mappedBy = "jedinka")
+    @JsonIgnore
     private List<IncidentJedinka> incidenti;
 }
